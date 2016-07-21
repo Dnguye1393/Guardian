f15dbe55c1d21eb4ef46ef3f1db73471f958b12e
David Nguyen <dnguyen1393@gmail.com>
2016-07-20 22:51:53 -0700

    Initial commit

---
+++ app/src/main/java/dnguyen1393/guardian/titleScreen.java
@@ -0,0 +1,13 @@
+package dnguyen1393.guardian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class titleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
